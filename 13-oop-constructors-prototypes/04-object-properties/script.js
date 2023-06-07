a2cbb82338b02e271c6806c644e84ef492d24268
Harrison King <Harrison.King@lowcarboncontracts.uk>
2023-06-07 08:43:12 +0100

    Object constructors completed

--- 13-oop-constructors-prototypes/04-object-properties/script.js
+++ 13-oop-constructors-prototypes/04-object-properties/script.js
@@ -13,17 +13,17 @@ const rect2 = new Rectangle('Rectangle 2', 20, 30);
 console.log(rect1.name, rect2.width);
 console.log(rect1['width']);
 
-// Add property
+// Add a new property to rect1
 rect1.color = 'red';
 
 rect2.perimeter = () => 2 * (rect2.width + rect2.height);
 
-// Delete property
+// Delete a property from rect2
 delete rect2.perimeter;
 
-// Check for property
-console.log(rect2.hasOwnProperty('color'));
+// Check if a property exists
 console.log(rect1.hasOwnProperty('color'));
+console.log(rect2.hasOwnProperty('color'));
 
 // Get keys
 console.log(Object.keys(rect1));
@@ -35,7 +35,10 @@ console.log(Object.values(rect2));
 console.log(Object.entries(rect1));
 
 for (let [key, value] of Object.entries(rect1)) {
-  if (typeof value !== 'function') {
-    console.log(`${key} - ${value}`);
-  }
+    if (typeof value !== 'function') {
+        console.log(`${key}: ${value}`);
+    }
 }
+
+
+
