e435adbf4b4a87e22f3fb755ba00aed72ec98e7d
imharrisonking <harrison.king@hotmail.co.uk>
2023-10-09 18:18:13 +0100

    Completes Iterators

--- 17-iterators-data-structures/02-iterators/script.js
+++ 17-iterators-data-structures/02-iterators/script.js
@@ -1,15 +1,15 @@
 // const app = {
-//   nextIndex: 0,
-//   teams: ['Red Sox', 'Yankees', 'Astros', 'Dodgers'],
-//   next() {
-//     if (this.nextIndex >= this.teams.length) {
-//       return { done: true };
-//     }
+//     nextIndex: 0,
+//     teams: ['Arsenal', 'Chelsea', 'City', 'United', 'Liverpool', 'Spurs'],
+//     next() {
+//         if (this.nextIndex >= this.teams.length) {
+//             return { done: true }
+//         }
 
-//     const returnValue = { value: this.teams[this.nextIndex], done: false };
-//     this.nextIndex++;
-//     return returnValue;
-//   },
+//         const returnValue = { value: this.teams[this.nextIndex], done: false};
+//         this.nextIndex++;
+//         return returnValue;
+//     },
 // };
 
 // console.log(app.next());
@@ -17,28 +17,32 @@
 // console.log(app.next());
 // console.log(app.next());
 // console.log(app.next());
+// console.log(app.next());
+// console.log(app.next());
 
 const app = {
-  teams: ['Red Sox', 'Yankees', 'Astros', 'Dodgers'],
-  [Symbol.iterator]: function () {
-    let nextIndex = 0;
-    return {
-      next: () => {
-        return nextIndex < this.teams.length
-          ? { value: this.teams[nextIndex++], done: false }
-          : { done: true };
-      },
-    };
-  },
-};
+    teams: ['Arsenal', 'Chelsea', 'City', 'United', 'Liverpool', 'Spurs'],
+    [Symbol.iterator]: function() {
+        let nextIndex = 0;
+        return {
+            next: () => {
+                return nextIndex < this.teams.length
+                ? {value: this.teams[nextIndex++], done: false}
+                : {done: true}
+            }
+        }
+    }
+}
 
 const iterator = app[Symbol.iterator]();
-// console.log(iterator.next().value);
-// console.log(iterator.next().value);
-// console.log(iterator.next().value);
-// console.log(iterator.next().value);
-// console.log(iterator.next());
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
 
-for (const team of app) {
-  console.log(team);
-}
+// for (const team of app) {
+//     console.log(team);
+// }
