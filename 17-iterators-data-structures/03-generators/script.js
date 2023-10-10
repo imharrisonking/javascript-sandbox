f5b53a4297d52435b148ac0a2682a7a196909633
imharrisonking <harrison.king@hotmail.co.uk>
2023-10-10 18:08:04 +0100

    Completes profile scroller

--- 17-iterators-data-structures/03-generators/script.js
+++ 17-iterators-data-structures/03-generators/script.js
@@ -1,27 +1,27 @@
-function* createTeamIterator(teams) {
-  for (let i = 0; i < teams.length; i++) {
-    yield teams[i];
-  }
+function * createTeamIterator(teams) {
+    for (let i = 0; i < teams.length; i++) {
+        yield teams[i];
+    }
 }
 
-const teams = ['Red Sox', 'Yankees', 'Astros', 'Dodgers'];
+const teams = ['Arsenal', 'Chelsea', 'City', 'United', 'Liverpool', 'Tottenham'];
 
 const iterator = createTeamIterator(teams);
 
-console.log(iterator.next().value); // Red Sox
-console.log(iterator.next().value); // Yankees
-console.log(iterator.next().value); // Astros
-console.log(iterator.next().value); // Dodgers
-console.log(iterator.next().done); // true
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
 
-// Use with for... of
-for (const team of createTeamIterator(teams)) {
-  console.log(team);
-}
+// for (const team of createTeamIterator(teams)) {
+//     console.log(teams);
+// }
 
-// Use with spread operator
-console.log([...createTeamIterator(teams)]);
+// console.log([...createTeamIterator(teams)])
 
-// Use with destructuring
 const [first, second, third] = createTeamIterator(teams);
+
 console.log(first, second, third);
--- 17-iterators-data-structures/04-profile-scroller-project/profile-scroller/script.js
+++ 17-iterators-data-structures/04-profile-scroller-project/profile-scroller/script.js
@@ -35,25 +35,25 @@ const people = [
 
 const container = document.querySelector('.container');
 const img = document.querySelector('img');
-const profileInfo = document.querySelector('.profile-info');
-const nextBtn = document.querySelector('#next');
+const profile_info = document.querySelector('.profile-info');
+const next_btn = document.querySelector('#next');
 
 function* createPeopleIterator() {
-  let index = 0;
-  while (true) {
-    yield people[index++ % people.length];
-  }
+    let index = 0;
+    while (true) {
+        yield people[index++ % people.length];
+    }
 }
 
 const iterator = createPeopleIterator();
 
-nextBtn.addEventListener('click', () => {
-  const person = iterator.next().value;
-  img.src = person.imageURL;
-  profileInfo.querySelector('h3').textContent = person.name;
-  profileInfo.querySelectorAll('p')[0].textContent = `${person.age} Years Old`;
-  profileInfo.querySelectorAll('p')[1].textContent = `From ${person.location}`;
-  profileInfo.querySelectorAll('p')[2].textContent = person.looking;
-});
+console.log(iterator.next());
 
-nextBtn.click();
+next_btn.addEventListener('click', () => {
+    const person = iterator.next().value;
+    img.src = person.imageURL;
+    profile_info.querySelector('h3').textContent = person.name;
+    profile_info.querySelectorAll('p')[0].textContent = `${person.age} Years Old`;
+    profile_info.querySelectorAll('p')[1].textContent = `From ${person.location}`;
+    profile_info.querySelectorAll('p')[2].textContent = person.looking;
+});
